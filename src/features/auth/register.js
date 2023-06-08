634ade4471ef46ed2bdc7bc444c5fa9ec6813a20
Temiloluwa Adeleye <temiloluwagithub@gmail.com>
2023-06-08 19:52:33 +0100

    setup register form

---
+++ src/features/auth/register.js
@@ -0,0 +1,15 @@
+const form = document.getElementsByTagName("form")[0];
+const [username, password, confirm_password] = [
+  document.getElementById("username"),
+  document.getElementById("password"),
+  document.getElementById("confirm_password")
+];
+
+function registerUser(e){
+    if(password.value !== confirm_password.value){
+        alert("Incorrect")
+    }
+    console.log({username: username.value, password: password.value})
+}
+
+form.addEventListener('submit', registerUser)
