d5e6c88a2677d95c9d8dda9c8321ee5051f09625
Temiloluwa Adeleye <temiloluwagithub@gmail.com>
2023-06-10 14:37:28 +0100

    added user session html implementation

--- main.js
+++ main.js
@@ -1,8 +1,16 @@
 import { createNavElement } from "./src/components/nav";
-
+import { userSession } from "./usersession";
 let nav = createNavElement();
-
 document.body.prepend(nav);
+console.log(userSession);
+let userName = document.querySelector("#user-name");
+let userEmail = document.querySelector("#user-email");
+if (userName && userSession)
+  userName.innerText = userSession.username;
+
+if (userEmail && userSession)
+  userName.innerText = userSession.email;
+
 let head = document.body.previousElementSibling;
 head.insertAdjacentHTML(
   "beforeend",
